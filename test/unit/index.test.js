2547a69c695a07b150e9ab7bf202d3e1d84fde52
Naveen Tamma <ntamma@hsdyn.com>
2025-06-11 12:19:42 +0100

    Suppress console output in production

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -30,3 +30,43 @@ describe('Init Script', () => {
     exitSpy.mockRestore()
   })
 })
+
+describe('Console Logging', () => {
+  let originalConsoleLog, originalConsoleDebug, originalConsoleInfo, originalConsoleWarn
+
+  beforeEach(() => {
+    originalConsoleLog = console.log
+    originalConsoleDebug = console.debug
+    originalConsoleInfo = console.info
+    originalConsoleWarn = console.warn
+  })
+
+  afterEach(() => {
+    console.log = originalConsoleLog
+    console.debug = originalConsoleDebug
+    console.info = originalConsoleInfo
+    console.warn = originalConsoleWarn
+  })
+
+  test('should disable console logging in non-development environments', () => {
+    process.env.NODE_ENV = 'production'
+    require('../../app/index')
+    expect(console.log).toEqual(expect.any(Function))
+    expect(console.log()).toBeUndefined()
+    expect(console.debug).toEqual(expect.any(Function))
+    expect(console.debug()).toBeUndefined()
+    expect(console.info).toEqual(expect.any(Function))
+    expect(console.info()).toBeUndefined()
+    expect(console.warn).toEqual(expect.any(Function))
+    expect(console.warn()).toBeUndefined()
+  })
+
+  test('should not disable console logging in development environments', () => {
+    process.env.NODE_ENV = 'development'
+    require('../../app/index')
+    expect(console.log).toEqual(originalConsoleLog)
+    expect(console.debug).toEqual(originalConsoleDebug)
+    expect(console.info).toEqual(originalConsoleInfo)
+    expect(console.warn).toEqual(originalConsoleWarn)
+  })
+})
