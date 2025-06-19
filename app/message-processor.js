d3239db45be80e322a53ae6c6ea94ae329bb2b9e
Naveen Tamma <ntamma@hsdyn.com>
2025-06-19 15:36:24 +0100

    pipeline upgrade to use v10

--- app/message-processor.js
+++ app/message-processor.js
@@ -46,14 +46,15 @@ class MessageProcessorService {
     let retryAttempts = 0
     let skipRetry = false
     const successMessage = 'Successfully connected to Azure Service Bus!'
-    const connectionString = process.env.SERVICE_BUS_CONNECTION_STRING
     const host = process.env.SERVICE_BUS_HOST
     const username = process.env.SERVICE_BUS_USERNAME
     const password = process.env.SERVICE_BUS_PASSWORD
+    const managedIdentityClientId = process.env.AZURE_CLIENT_ID
+    const credential = new DefaultAzureCredential({managedIdentityClientId: process.env.AZURE_CLIENT_ID});
 
     while (retryAttempts < retries) {
       try {
-        if (connectionString) {
+        /*if (connectionString) {
           this.serviceBusClient = new ServiceBusClient(connectionString)
           console.log(successMessage)
           return
@@ -61,8 +62,9 @@ class MessageProcessorService {
           this.serviceBusClient = new ServiceBusClient(`Endpoint=sb://${host}/;SharedAccessKeyName=${username};SharedAccessKey=${password}`)
           console.log(successMessage)
           return
-        } else if (host) {
-          this.serviceBusClient = new ServiceBusClient(host, new DefaultAzureCredential())
+        } else*/ 
+        if (host && credential) {
+          this.serviceBusClient = new ServiceBusClient(host, credential)
           console.log(successMessage)
           return
         } else {
